0859cd66844996f3ff8c0b9e4d983a5535ef1e42
rmb1952 <rmb1952@rit.edu>
2016-02-19 19:40:36 -0500

    Added buildings to scene, created script to make objects draggable

---
+++ LEEDCertificationGame/Assets/Scripts/Draggable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Draggable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDrag()
+	{
+		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+		Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+		transform.position = new Vector3(pos_move.x, pos_move.y, transform.position.z);
+	}
+}
