a42fb3f2c82f5aab5ea5e563ad410c00a9b49185
rmb1952 <rmb1952@rit.edu>
2016-02-20 18:30:22 -0500

    Added GameManager script and prefabs for draggables

---
+++ LEEDCertificationGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject bikeRackPrefab;
+	public GameObject ventilationPrefab;
+	public GameObject wasteManagementPrefab;
+	public GameObject appliancesPrefab;
+	public GameObject insulationPrefab;
+	public GameObject lightingPrefab;
+	public GameObject windowsPrefab;
+	public GameObject geothermalPrefab;
+	public GameObject showerPrefab;
+	public GameObject solarPanelPrefab;
+	public GameObject drainagePrefab;
+	public GameObject irrigationPrefab;
+	public GameObject landscapingPrefab;
+	public GameObject windTurbinePrefab;
+
+	public GameObject bikeRackText;
+	public GameObject ventilationText;
+	public GameObject wasteManagementText;
+	public GameObject appliancesText;
+	public GameObject insulationText;
+	public GameObject lightingText;
+	public GameObject windowsText;
+	public GameObject geothermalText;
+	public GameObject showerText;
+	public GameObject solarPanelText;
+	public GameObject drainageText;
+	public GameObject irrigationText;
+	public GameObject landscapingText;
+	public GameObject windTurbineText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
