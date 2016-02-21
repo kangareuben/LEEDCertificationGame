4cedead979d55c8b06198e15cdce2bab341fae25
rmb1952 <rmb1952@rit.edu>
2016-02-20 23:03:23 -0500

    Added incrementing and decrementing of text, switched to lists instead of individual GameObjects

--- LEEDCertificationGame/Assets/Scripts/Draggable.cs
+++ LEEDCertificationGame/Assets/Scripts/Draggable.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class Draggable : MonoBehaviour {
 
+	bool decrementedText = false;
+	GameManager gm;
+	int index;
+
+	public int Index
+	{
+		get{return index;}
+		set{index = value;}
+	}
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+		gm = GameObject.Find("GameManager").GetComponent<GameManager>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 	
 	}
 
@@ -18,5 +30,16 @@ public class Draggable : MonoBehaviour {
 		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
 		Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
 		transform.position = new Vector3(pos_move.x, pos_move.y, transform.position.z);
+
+		if(!decrementedText)
+		{
+			decrementedText = true;
+			gm.IncrementOrDecrementText(index, -1);
+		}
+	}
+
+	void OnMouseUpAsButton()
+	{
+		Debug.Log("mouse released");
 	}
 }
--- LEEDCertificationGame/Assets/Scripts/GameManager.cs
+++ LEEDCertificationGame/Assets/Scripts/GameManager.cs
@@ -1,42 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
-	public GameObject bikeRackPrefab;
-	public GameObject ventilationPrefab;
-	public GameObject wasteManagementPrefab;
-	public GameObject appliancesPrefab;
-	public GameObject insulationPrefab;
-	public GameObject lightingPrefab;
-	public GameObject windowsPrefab;
-	public GameObject geothermalPrefab;
-	public GameObject showerPrefab;
-	public GameObject solarPanelPrefab;
-	public GameObject drainagePrefab;
-	public GameObject irrigationPrefab;
-	public GameObject landscapingPrefab;
-	public GameObject windTurbinePrefab;
+	public List<GameObject> iconPrefabs = new List<GameObject>();
 
-	public GameObject bikeRackText;
-	public GameObject ventilationText;
-	public GameObject wasteManagementText;
-	public GameObject appliancesText;
-	public GameObject insulationText;
-	public GameObject lightingText;
-	public GameObject windowsText;
-	public GameObject geothermalText;
-	public GameObject showerText;
-	public GameObject solarPanelText;
-	public GameObject drainageText;
-	public GameObject irrigationText;
-	public GameObject landscapingText;
-	public GameObject windTurbineText;
+	public List<GameObject> iconTexts = new List<GameObject>();
 
-	public GameObject building1Text;
-	public GameObject building2Text;
-	public GameObject building3Text;
+	public List<GameObject> buildingTexts = new List<GameObject>();
 
+	List<int> iconValues = new List<int>();
 	int bikeRackValue = 1;
 	int ventilationValue = 8;
 	int wasteManagementValue = 3;
@@ -53,7 +28,8 @@ public class GameManager : MonoBehaviour {
 	int windTurbineValue = 3;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		ventilationValue = Random.Range(1, ventilationValue + 1);
 		wasteManagementValue = Random.Range(1, wasteManagementValue + 1);
 		appliancesValue = Random.Range(1, appliancesValue + 1);
@@ -64,10 +40,57 @@ public class GameManager : MonoBehaviour {
 		drainageValue = Random.Range(1, drainageValue + 1);
 		irrigationValue = Random.Range(1, irrigationValue + 1);
 		landscapingValue = Random.Range(1, landscapingValue + 1);
+
+		iconValues.Add(bikeRackValue);
+		iconValues.Add(ventilationValue);
+		iconValues.Add(wasteManagementValue);
+		iconValues.Add(appliancesValue);
+		iconValues.Add(insulationValue);
+		iconValues.Add(lightingValue);
+		iconValues.Add(windowsValue);
+		iconValues.Add(geothermalValue);
+		iconValues.Add(showerValue);
+		iconValues.Add(solarPanelValue);
+		iconValues.Add(drainageValue);
+		iconValues.Add(irrigationValue);
+		iconValues.Add(landscapingValue);
+		iconValues.Add(windTurbineValue);
+
+		CreatePuzzle();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 	
+	}
+
+	void CreatePuzzle()
+	{
+		int totalPoints = 0;
+
+		for(int i = 0; i < 30; i++)
+		{
+			int index = Random.Range(0, iconPrefabs.Count);
+
+			GameObject g = Object.Instantiate(iconPrefabs[index], iconPrefabs[index].transform.position, iconPrefabs[index].transform.rotation) as GameObject;
+			g.AddComponent<Draggable>();
+			g.GetComponent<Draggable>().Index = index;
+			g.AddComponent<BoxCollider2D>();
+
+			IncrementOrDecrementText(index, 1);
+
+			totalPoints += iconValues[index];
+		}
+
+		Debug.Log(totalPoints);
+	}
+
+	public void IncrementOrDecrementText(int index, int value)
+	{
+		string s = iconTexts[index].GetComponent<Text>().text.Substring(0, iconTexts[index].GetComponent<Text>().text.Length - 1);
+		int num = int.Parse(iconTexts[index].GetComponent<Text>().text.Substring(iconTexts[index].GetComponent<Text>().text.Length - 1));
+		num += value;
+		iconTexts[index].GetComponent<Text>().text = s + num.ToString();
 	}
 }
