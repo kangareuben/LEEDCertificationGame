f93ad457c09b1c70425a94fba8bae690fef6d11c
rmb1952 <rmb1952@rit.edu>
2016-02-23 00:16:21 -0500

    Minor build tweaks

--- LEEDCertificationGame/Assets/Scripts/GameManager.cs
+++ LEEDCertificationGame/Assets/Scripts/GameManager.cs
@@ -84,6 +84,11 @@ public class GameManager : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+
 		if(iconsDraggable){
 			CheckForWin();
 		}
--- LEEDCertificationGame/Assets/Scripts/MenuManager.cs
+++ LEEDCertificationGame/Assets/Scripts/MenuManager.cs
@@ -12,7 +12,10 @@ public class MenuManager : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-	
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
 	}
 
 	public void Credits()
